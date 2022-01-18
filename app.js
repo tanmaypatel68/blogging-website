b24b646a4e4be242b2d4be1b4702e9a684029c84
tanmay <patelt9238@gmail.com>
2022-01-18 16:46:50 +0530

    worked on compose and home page

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts=[];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    res.render("home",{startingContent:homeStartingContent});
+    res.render("home",{startingContent:homeStartingContent,
+    posts:posts});
 });
 
 app.get("/about",function(req,res){
@@ -27,7 +30,16 @@ app.get("/contact",function(req,res){
 
 app.get("/compose",function(req,res){
     res.render("compose");
-})
+});
+
+app.post("/compose",function(req,res){
+    const post={
+        title:req.body.postTitle,
+        content:req.body.postBody
+    }
+    posts.push(post);
+    res.redirect("/");
+});
 
 app.listen(3000,function(){
     console.log("server started on port 3000");
